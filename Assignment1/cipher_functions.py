35d96f8b797eeba4266876c3ca786d5c28748ccd
dmf444 <becat444@gmail.com>
2016-11-01 09:14:04 -0400

    Added first function

--- Assignment1/cipher_functions.py
+++ Assignment1/cipher_functions.py
@@ -5,3 +5,20 @@ JOKER1 = 27
 JOKER2 = 28
 
 # Write your functions here:
+
+
+def clean_message(message):
+    '''
+    this function takes in a String message and cuts out all spaces, then puts
+    everything in uppercase. Returns the new string
+    (str) -> str
+    REQ: message != null, message can only contain alphabetic charaters
+    >>> clean_message("no body knows the trouble ive seen")
+    'NOBODYKNOWSTHETROUBLEIVESEEN'
+    >>> clean_message("FHSLR D")
+    'FHSLRD'
+    '''
+    new_message = message.replace(" ", "")
+    new_message = new_message.upper()
+    return new_message
+
