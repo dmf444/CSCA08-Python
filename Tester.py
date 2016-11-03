3ec74c62582c868622e9787acdc7374c280d8b0d
dmf444 <becat444@gmail.com>
2016-11-03 09:55:25 -0400

    Added some in class code from WEEK 8 - Part 1

--- Tester.py
+++ Tester.py
@@ -1,29 +1,55 @@
-def func1(var1, var2):
-    print("func1: ", var1, var2)
-    return var3
-def func2(var1, var2):
-    var3 = func1(var1, var2)
-    print("func2: ", var1, var2, var3)
-    return var1 + var2
-def func3(var1, var2, var3):
-    var1 = func2(var1, var2)
-    var2 = func2(var2, var3)
-    print("func3: ", var1, var2, var3)
-    return func2(func1(var1, var2), func1(var2, var1))
+class Person():
+    '''More strings for reasons unknown'''
+    ###############################
+    #
+    # ALL INTERNAL VARS SHOULD BE PRIVVY
+    #
+    ###############################
+    def __init__(self, name, age, gender):
+        # print("I'm in the init method")
+        self.set_name(name)
+        self.set_age(age)
+        self.set_gender(gender)
 
-var1 = "A"
-var2 = "B"
-var3 = "C"
-print("STEP 1: ", var1, var2, var3)
-var3 = func1(var1, var2)
-print("STEP 2: ", var1, var2, var3)
-var1 = "A"
-var2 = "B"
-var3 = "C"
-var3 = func2(var1, var2)
-print("STEP 3: ", var1, var2, var3)
-var1 = "A"
-var2 = "B"
-var3 = "C"
-var3 = func3(var1, var2,var3)
-print("STEP 4: ", var1, var2, var3)
+    def set_age(self, new_age):
+        self._age = new_age
+
+    def set_name(self, new_name):
+        self._name = new_name
+
+    def set_gender(self, new_gender):
+        if(new_gender == "Male"):
+            self._is_male = True
+        else:
+            self._is_male = False
+
+    def get_age(self):
+        return self._age
+
+    def get_name(self):
+        return self._name
+
+    def get_gender(self):
+        if(self._is_male):
+            r = "Male"
+        else:
+            r = "Female"
+        return r
+
+############################################
+
+if(__name__ == "__main__"):
+    person1 = Person("Alice Smith", 30, "Female")
+    # person1.set_name("Alice")
+    # person1.set_gender("Female")
+    person2 = Person("Bob", 18, "Male")
+    # person2.set_name("Bob")
+    # person2.set_gender("Male")
+    population = {person1, person2}
+    for next_person in population:
+        greeting = "Hello "
+        if(next_person.get_gender() == "Female"):
+            greeting += "Ms. "
+        else:
+            greeting += "Mr."
+        print(greeting + next_person.get_name())
