9fa2757959b59055f45049a82cf0b02c10ea5ec0
dmf444 <becat444@gmail.com>
2017-03-02 23:13:48 -0500

    Finished ex6. I is ded

--- CSCA48/ex6/ex6.py
+++ CSCA48/ex6/ex6.py
@@ -43,7 +43,79 @@ def rmax(in_list: list) -> int:
     return count
 
 
+def sum_max_min(inlist: list)-> int:
+    """
+    This function finds the sum of the largest and smallest numbers in a list.
+    Function returns the sum of the numbers in the list
+    """
+    # Call helper fuction, find largest and smallest
+    (smallest, biggest) = max_min_helper(inlist)
+    # Add the two together
+    added = smallest + biggest
+    # Return
+    return added
 
-print(rmax([1, 2, 3]))
-print(rmax([1, [2, 3]]))
-print(rmax([[1], [2, [3]], [], [-4,5,6,7,8]]))
+
+def max_min_helper(inlist: list) -> (int, int):
+    """
+    This function takes in a list and computes the max and minimum numbers
+    of the list, then returns a tuple containing those numbers.
+    :param inlist: list containing numbers and lists
+    :return: Tuple containing the largest and smallest number
+    """
+    if(len(inlist) == 1):
+        if(isinstance(inlist[0], list)):
+            count = max_min_helper(inlist[0])
+        else:
+            count = (inlist[0], inlist[0])
+    elif(len(inlist) >= 2):
+        var = len(inlist)//2
+        one = max_min_helper(inlist[:var])
+        two = max_min_helper(inlist[var:])
+        if(two[1] > one[1]):
+            one = (one[0], two[1])
+        if(two[0] < one[0]):
+            one = (two[0], one[1])
+        count = one
+    else:
+        count = (float("inf"), float("-inf"))
+    return count
+
+
+def second_smallest(in_list: list) -> int:
+    """
+    This function takes in a list and finds the second smallest
+    value inside the list. This function operates with nested lists.
+    REQ: nested list may only contain lists and ints
+    RETURNS: second_smallest number in the list
+    """
+    (smallest, second_smaller) = second_smallest_helper(in_list)
+    if(second_smaller == float("inf") or second_smaller == float("-inf")):
+        second_smaller = smallest
+    return second_smaller
+
+
+def second_smallest_helper(inlist: list) -> (int, int):
+    """
+    Returns the two smallest numbers in the list. Uses recursion over the
+    .sort method
+    :param inlist: nested list, containing only ints and lists
+    :return: tuple of the two smallest ints
+    """
+    if (len(inlist) == 1):
+        if (isinstance(inlist[0], list)):
+            count = second_smallest_helper(inlist[0])
+        else:
+            count = (inlist[0], inlist[0])
+    elif (len(inlist) >= 2):
+        half_pt = len(inlist) // 2
+        first_cut = second_smallest_helper(inlist[:half_pt])
+        second_cut = second_smallest_helper(inlist[half_pt:])
+        if (first_cut[0] < second_cut[0]):
+            first_cut = (first_cut[0], second_cut[0])
+        elif (first_cut[0] > second_cut[0]):
+            first_cut = (second_cut[0], first_cut[0])
+        count = first_cut
+    else:
+        count = (float("inf"), float("-inf"))
+    return count
