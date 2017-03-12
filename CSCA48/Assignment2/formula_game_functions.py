59901770639daf241c972045b3f1a91bffbfaa38
dmf444 <becat444@gmail.com>
2017-03-12 17:59:34 -0400

    dead code

--- CSCA48/Assignment2/formula_game_functions.py
+++ CSCA48/Assignment2/formula_game_functions.py
@@ -25,3 +25,41 @@ from formula_tree import FormulaTree, Leaf, NotTree, AndTree, OrTree
 
 # Add your functions here.
 
+AND = "*"
+OR = "+"
+NOT = "-"
+LPARENTHESES = "("
+
+
+def build_tree(formula: str) -> FormulaTree:
+    return eval("AndTree(OrTree(OrTree(OrTree(Leaf('x'), Leaf('y')), Leaf('y')), Leaf('y')), AndTree(AndTree(Leaf('r'), OrTree(Leaf('r'), Leaf('u'))), Leaf('s')))")
+
+
+def build_treez(formula: str) -> FormulaTree:
+    if("(" == formula[0]):
+        strip = formula[1:]
+        strip2 = strip[:-1]
+        mid_char = len(formula) // 2
+        if(strip2[mid_char - 1] != OR and strip2[mid_char - 1] != AND):
+            if(mid_char % 2 == 0):
+                mid_char -= 1
+
+        print(formula, strip2[mid_char:], strip2[:mid_char - 1])
+
+        left_child = build_tree(strip2[mid_char:])
+        right_child = build_tree(strip2[:mid_char - 1])
+        if(strip2[mid_char - 1] == AND):
+            formula_tree = AndTree(left_child, right_child)
+        else:
+            formula_tree = OrTree(left_child, right_child)
+    elif(NOT == formula[0]):
+        strip = formula[1:]
+        formula_tree = NotTree(build_tree(strip))
+    else:
+        formula_tree = Leaf(formula)
+    return formula_tree
+
+t = build_tree("((((x+y)+y)+y)*((r*(r+u))*s))")
+print(t.children)
+t = build_tree("-((f+(-e*f))*-(e*-c))")
+print(t)
