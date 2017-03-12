71065c21974d51ec6a58c3a2ad184ebea859c76a
dmf444 <becat444@gmail.com>
2017-03-12 19:54:02 -0400

    Added in one function for A2

--- CSCA48/Assignment2/formula_game_functions.py
+++ CSCA48/Assignment2/formula_game_functions.py
@@ -1,5 +1,5 @@
 """
-# Copyright Nick Cheng, 2016
+# Copyright Nick Cheng, 2016; David Fernandes 2017
 # Distributed under the terms of the GNU General Public License.
 #
 # This file is part of Assignment 2, CSCA48, Winter 2017
@@ -29,37 +29,135 @@ AND = "*"
 OR = "+"
 NOT = "-"
 LPARENTHESES = "("
+RPARENTHESES = ")"
 
 
 def build_tree(formula: str) -> FormulaTree:
-    return eval("AndTree(OrTree(OrTree(OrTree(Leaf('x'), Leaf('y')), Leaf('y')), Leaf('y')), AndTree(AndTree(Leaf('r'), OrTree(Leaf('r'), Leaf('u'))), Leaf('s')))")
-
-
-def build_treez(formula: str) -> FormulaTree:
-    if("(" == formula[0]):
-        strip = formula[1:]
-        strip2 = strip[:-1]
-        mid_char = len(formula) // 2
-        if(strip2[mid_char - 1] != OR and strip2[mid_char - 1] != AND):
-            if(mid_char % 2 == 0):
-                mid_char -= 1
-
-        print(formula, strip2[mid_char:], strip2[:mid_char - 1])
-
-        left_child = build_tree(strip2[mid_char:])
-        right_child = build_tree(strip2[:mid_char - 1])
-        if(strip2[mid_char - 1] == AND):
-            formula_tree = AndTree(left_child, right_child)
-        else:
-            formula_tree = OrTree(left_child, right_child)
-    elif(NOT == formula[0]):
-        strip = formula[1:]
-        formula_tree = NotTree(build_tree(strip))
+    ''' (str) -> FormulaTree
+    This function takes in a string, representing a boolean formula and
+    returns the valid tree structure of that formula. If the function is
+    invalid, the function will return None. Valid formula are as follows:
+    1) All variables (single letters) must be lower case
+    2) And/Or signs must have paretheses on either side of the variables
+    3) Not signs must be directly applied to the variables
+    4) Single variables cannot have parentheses
+    REQ: len(formula) > 0
+    REQ: formula exclusively contains lowercase letters, *, +, -, (, ) only
+    >>> a = build_tree("x+y")
+    >>> print(a == None)
+    True
+    >>> b = build_tree("-(x)")
+    >>> print(b == None)
+    True
+    >>> c = build_tree("(x+Y)")
+    >>> print(c == None)
+    True
+    >>> d = build_tree("(x+(y)*z)")
+    >>> print(d == None)
+    True
+    >>> e = build_tree("(4+5)")
+    >>> print(e == None)
+    True
+    >>> f = build_tree("(a+d)")
+    >>> print(eval("OrTree(Leaf('a'), Leaf('d'))") == f)
+    True
+    '''
+    # First, check if all variables are lowercase within the function
+    if(formula.islower()):
+        try:
+            # Attempt to use recursion to make the tree
+            tree = tree_builder(formula)
+        # Extraneous parentheses and mismatched parentheses will crash the
+        # build function with an IndexError. Catch that error and return None,
+        # because formula is invalid.
+        except IndexError:
+            tree = None
+    # The variables contained an upper case - the formula is invalid
     else:
-        formula_tree = Leaf(formula)
+        tree = None
+    return tree
+
+
+def tree_builder(formula: str) -> FormulaTree:
+    # Check if the formula starts with a not symbol
+    if(formula[0] == NOT):
+        # Recursively call the tree again.
+        child_tree = tree_builder(formula[1:])
+        # This line will get changed, unless the child_tree returns None
+        formula_tree = child_tree
+        # Check if the child tree is not None (all sub trees were valid)
+        if(child_tree is not None):
+            # Add the sub tree to a not root, then return
+            formula_tree = NotTree(child_tree)
+    # Check if the formula starts with a brace, signifying a +/* upcoming
+    elif(formula.startswith("(")):
+        # Strip off the braces from the formula (we assume it's valid)
+        stripped_brackets = formula[1:-1]
+        # Create a variable to count the number of braces, midpoint and
+        # if the midpoint has been found
+        mid_point = 0
+        brace_count = 0
+        is_found = False
+        # NOTE: How this works - Assume valid. Braces must open and close.
+        # Count all openings, and all closing. When that count is 0,
+        # we've found our midpoint, representing * or +.
+        # Loop until midpoint is found
+        while(not is_found):
+            # Should we find a Left Parentheses, add 1 to the counter
+            if(stripped_brackets[mid_point] == LPARENTHESES):
+                brace_count += 1
+            # If we find a Right parentheses, remove 1 from the counter
+            elif(stripped_brackets[mid_point] == RPARENTHESES):
+                brace_count -= 1
+            # If we hit 0 on the brace count and we've found an */+
+            if(brace_count == 0 and (stripped_brackets[mid_point] == AND or
+                                     stripped_brackets[mid_point] == OR)):
+                # Exit the loop
+                is_found = True
+            else:
+                # Otherwise, increase the midpoint index, keep searching
+                mid_point += 1
+        # Assuming the midpoint was found, everything after it's the right tree
+        # everything before it is the left tree
+        # Recursively call the build function with that parameter.
+        left_tree = tree_builder(stripped_brackets[:mid_point])
+        right_tree = tree_builder(stripped_brackets[mid_point + 1:])
+        # Default the return to None, incase one of the child nodes is None
+        formula_tree = None
+        # Check if the child nodes are None
+        if(left_tree is not None and right_tree is not None):
+            # If they're not, figure out what the midpoint symbol was
+            # If it's the AND
+            if(stripped_brackets[mid_point] == AND):
+                # Create an AND tree
+                formula_tree = AndTree(left_tree, right_tree)
+            else:
+                # Otherwise, create an OR tree
+                formula_tree = OrTree(left_tree, right_tree)
+    # Not an bracket nor a Not, either leaf or invalid
+    else:
+        # If this is a leaf, the length should be 1
+        if(len(formula) > 1):
+            # Found a length greater than 1, formula has an error
+            formula_tree = None
+        else:
+            # Return a FormulaTree with the leaf of the variable
+            formula_tree = Leaf(formula)
     return formula_tree
 
-t = build_tree("((((x+y)+y)+y)*((r*(r+u))*s))")
-print(t.children)
-t = build_tree("-((f+(-e*f))*-(e*-c))")
-print(t)
+
+if(__name__ == "__main__"):
+    t = build_tree("((((x+y)+y)+y)*((r*(r+u))*s))")
+    print(t)
+    t = build_tree("-((f+(-e*f))*-(e*-c))")
+    print(t)
+    t = build_tree("(((x+y)*(y+z))*(-y+-z))")
+    print(t)
+    o = build_tree("(a+(b+(c+(d+(e+(f+(g+(h+(i+(j+(k+(l+(m+(n+(o+(p+(q+(r+(s+(t+(u+(v+(w+(x+(y+z)))))))))))))))))))))))))")
+    l = eval("OrTree(Leaf('a'), OrTree(Leaf('b'), OrTree(Leaf('c'), OrTree(Leaf('d'), OrTree(Leaf('e'), OrTree(Leaf('f'), OrTree(Leaf('g'), OrTree(Leaf('h'), OrTree(Leaf('i'), OrTree(Leaf('j'), OrTree(Leaf('k'), OrTree(Leaf('l'), OrTree(Leaf('m'), OrTree(Leaf('n'), OrTree(Leaf('o'), OrTree(Leaf('p'), OrTree(Leaf('q'), OrTree(Leaf('r'), OrTree(Leaf('s'), OrTree(Leaf('t'), OrTree(Leaf('u'), OrTree(Leaf('v'), OrTree(Leaf('w'), OrTree(Leaf('x'), OrTree(Leaf('y'), Leaf('z'))))))))))))))))))))))))))")
+    print(o == l)
+    t = build_tree("(a+d)")
+    print(t)
+
+    import doctest
+    doctest.testmod()
