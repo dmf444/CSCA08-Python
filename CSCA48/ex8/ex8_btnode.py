3cbbe2a296c59926acaa0dad40d524d5dc250225
dmf444 <becat444@gmail.com>
2017-03-27 12:23:07 -0400

    Added pdf question

---
+++ CSCA48/ex8/ex8_btnode.py
@@ -0,0 +1,36 @@
+class BTNode(object):
+    """A node in a binary tree."""
+
+    def __init__(self, value, left=None, right=None):
+        """(BTNode, int, BTNode, BTNode) -> NoneType
+        Initialize this node to store value and have children left and right,
+        as well as depth 0.
+        """
+        self.value = value
+        self.left = left
+        self.right = right
+        self.d = 0
+
+    def __str__(self):
+        return self._str_helper("")
+
+    def _str_helper(self, indentation=""):
+        """(BTNode, str) -> str
+        Return a "sideways" representation of the subtree rooted at this node,
+        with right subtrees above parents above left subtrees and each node on
+        its own line, preceded by as many TAB characters as the node's depth.
+        """
+        ret = ""
+
+        if(self.right is not None):
+            ret += self.right._str_helper(indentation + "\t") + "\n"
+        ret += indentation + str(self.value) + "\n"
+        if(self.left is not None):
+            ret += self.left._str_helper(indentation + "\t") + "\n"
+        return ret
+
+if(__name__ == "__main__"):
+    # just a simple tree to practice on
+    my_tree = BTNode(10, BTNode(3, BTNode(5), BTNode(2)),
+                     BTNode(7, BTNode(4, BTNode(9)), BTNode(6)))
+    print(my_tree)
